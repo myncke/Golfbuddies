c8e975bf6c4d479abfbe92b5033ca87e46231358
myncke <ohsab@github.com>
2017-10-12 12:06:04 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,13 @@
       <router-view></router-view>
     </main> -->
 
-    <v-app>
-      <v-navigation-drawer app></v-navigation-drawer>
-      <v-toolbar app></v-toolbar>
+    <!-- <v-app>
+      <v-navigation-drawer app>
+        Test
+      </v-navigation-drawer>
+      <v-toolbar app>
+        Golfbuddies
+      </v-toolbar>
       <main>
         <v-content>
           <v-container fluid>
@@ -19,13 +23,55 @@
         </v-content>
       </main>
       <v-footer app></v-footer>
-    </v-app>
 
-    <!-- <v-app>
-      <main>
-        <v-container>Hello world</v-container>
-      </main>
+
+      <v-botttom-nav></v-botttom-nav>
     </v-app> -->
+
+    <v-app id="example-1">
+      <v-navigation-drawer persistent v-model="drawer">
+        <v-list>
+            <v-list-tile>
+                <v-list-tile-content>
+                    <v-list-tile-title >
+                        <span>Menu</span>
+                    </v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
+            <v-divider></v-divider>
+            <template v-for="(item, index) in items">
+                <v-list-tile :href="item.href" :to="{name: item.href}">
+                    <v-list-tile-action>
+                        <v-icon light v-html="item.icon"></v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </template>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-toolbar class="indigo" dark>
+          <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+          <v-toolbar-title>Golfbuddies</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn icon @click.native.stop="openGithub()">
+              <v-icon>home</v-icon>
+          </v-btn>
+      </v-toolbar>
+
+      <main>
+          <v-fade-transition mode="out-in">
+              <router-view></router-view>
+          </v-fade-transition>
+      </main>
+
+      <v-footer class="indigo" dark>
+          <span>© 2017 - disjfa</span>
+      </v-footer>
+      
+    </v-app>
   </div>
 </template>
 
