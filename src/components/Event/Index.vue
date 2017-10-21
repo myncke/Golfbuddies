7522b94dcd88f180e726c4eacfcebcdb486d0a5f
myncke <ohsab@github.com>
2017-10-21 23:54:56 +0200

    parallax test

--- src/components/Event/Index.vue
+++ src/components/Event/Index.vue
@@ -1,93 +1,75 @@
 <template>
   <v-container fluid class="ma-0 pa-0">
     <v-layout>
-      <v-flex row justify-center>
+      <v-flex>
 
-        <v-container fluid class="ma-0 pa-0">
+        <v-container fluid class="ma-0 pa-0" >
           <v-layout>
             <v-flex>
-              <!-- <v-toolbar color="white" class="elevation-0">
-                
-              </v-toolbar> -->
-              <v-container fluid class="ma-0 pa-0" >
-                <v-layout>
-                  <v-flex>
-                    <v-expansion-panel>
-                      <v-expansion-panel-content v-model="open">
-                        <div slot="header">
-                          <v-icon class="mr-3" left>filter_list</v-icon> Filters
-                          <!-- <v-icon class="ml-5">event</v-icon>
-                          <span class="ml-3">Oct.</span>
-                          <span class="ml-1">2017</span> -->
-                        </div>
-                        <v-card>
-                          <v-card-text class="grey lighten-5">
-                            <v-layout row wrap>
-                              <v-flex sx12 sm5>
-                                <menu-date-picker></menu-date-picker>
-                                <menu-date-picker></menu-date-picker>
+              <v-expansion-panel>
+                <v-expansion-panel-content v-model="open">
+                  <div slot="header">
+                    <v-icon class="mr-3" left>filter_list</v-icon> Filters
+                    <!-- <v-icon class="ml-5">event</v-icon>
+                    <span class="ml-3">Oct.</span>
+                    <span class="ml-1">2017</span> -->
+                  </div>
+                  <v-card>
+                    <v-card-text class="grey lighten-5">
+                      <v-layout row wrap>
+                        <v-flex sx12 sm5>
+                          <menu-date-picker></menu-date-picker>
+                          <menu-date-picker></menu-date-picker>
 
-                                 <v-layout wrap>
-                                  <v-flex xs12>
-                                    <v-select
-                                      v-model="select"
-                                      label="Extra Filters"
-                                      chips
-                                      tags
-                                      :items="items"
-                                    ></v-select>
-                                  </v-flex>
-                                </v-layout>
-                              </v-flex>
+                            <v-layout wrap>
+                            <v-flex xs12>
+                              <v-select
+                                v-model="select"
+                                label="Extra Filters"
+                                chips
+                                tags
+                                :items="items"
+                              ></v-select>
+                            </v-flex>
+                          </v-layout>
+                        </v-flex>
 
-                              <v-flex sm5 offset-sm1>
-                                <v-layout row wrap>
-                                  <v-flex xs10>
-                                   <v-slider prepend-icon="group" :max="8" v-model="group_size"></v-slider>
-                                  </v-flex>
-                                  <v-flex xs2>
-                                    <v-text-field v-model="group_size" type="number"></v-text-field>
-                                  </v-flex>
-                                  <v-flex xs10>
-                                   <v-slider prepend-icon="accessible" :max="50" v-model="hdc"></v-slider>
-                                  </v-flex>
-                                  <v-flex xs2>
-                                    <v-text-field v-model="hdc" type="number"></v-text-field>
-                                  </v-flex>
+                        <v-flex sm5 offset-sm1>
+                          <v-layout row wrap>
+                            <v-flex xs10>
+                              <v-slider prepend-icon="group" :max="8" v-model="group_size"></v-slider>
+                            </v-flex>
+                            <v-flex xs2>
+                              <v-text-field v-model="group_size" type="number"></v-text-field>
+                            </v-flex>
+                            <v-flex xs10>
+                              <v-slider prepend-icon="accessible" :max="50" v-model="hdc"></v-slider>
+                            </v-flex>
+                            <v-flex xs2>
+                              <v-text-field v-model="hdc" type="number"></v-text-field>
+                            </v-flex>
 
-                                  <v-flex>
-                                    <v-select
-                                      v-bind:items="sex"
-                                      v-model="e1"
-                                      label="Select"
-                                      single-line
-                                      auto
-                                      prepend-icon="accessibility"
-                                      hide-details
-                                    ></v-select>
-                                  </v-flex>
-                                </v-layout>
-                              </v-flex>
-
-                              <v-flex sm3>
-                                
-                              </v-flex>
-
-                            </v-layout>
-
-
-                          </v-card-text>
-                        </v-card>
-                      </v-expansion-panel-content>
-                    </v-expansion-panel>
-                  </v-flex>
-                </v-layout>
-              </v-container>
+                            <v-flex>
+                              <v-select
+                                v-bind:items="sex"
+                                v-model="e1"
+                                label="Select"
+                                single-line
+                                auto
+                                prepend-icon="accessibility"
+                                hide-details
+                              ></v-select>
+                            </v-flex>
+                          </v-layout>
+                        </v-flex>
+                      </v-layout>
+                    </v-card-text>
+                  </v-card>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
             </v-flex>
           </v-layout>
         </v-container>
-
-        <!-- <v-divider></v-divider> -->
 
         <index-tabs></index-tabs>
       </v-flex>
--- src/components/Group/Show.vue
+++ src/components/Group/Show.vue
@@ -1,45 +1,58 @@
 <template>
-  <div style="width: 100%; margin-bottom: 80px"  v-if="club !== undefined">
-    <img :src="club.headerPic" class="header-img"/>
-    <v-card>
-      <v-card-title primary-title>
-        <p class="text-xs-center headline" style="width: 100%">{{club.name}}</p>
-      </v-card-title>
-      <v-card-text style="margin: 10px">
-        <p>{{club.information}}</p>
-        <v-divider style="margin: 10px"></v-divider>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn icon @click.native="showMembers = !showMembers; showLocation = false">
-          <v-icon>{{ showMembers ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
-        </v-btn>
-        <p>Members</p>
-        <v-spacer></v-spacer>
-        <p>Location</p>
-        <v-btn icon @click.native="showLocation = !showLocation; showMembers = false">
-          <v-icon>{{ showLocation ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
-        </v-btn>
-      </v-card-actions>
-      <v-slide-y-transition>
-        <v-card-text v-show="showLocation">
-          <location-view v-if="club.location !== undefined" :location="club.location"></location-view>
+  <v-container v-if="club !== undefined" class="pa-0 ma-0" fluid>
+    <v-layout column>
+      <v-flex>
+        <v-card>
+          <v-card-media :src="club.headerPic" height="400px">
+            <v-parallax :src="club.headerPic" class="resize-parallax">
+              <v-layout column align-left justify-end style="width:100%;">
+                <!-- <h1 class="white--text">{{club.name}}</h1>
+                <h4 class="white--text">Build your application today!</h4> -->
+              </v-layout>
+            </v-parallax>
+          </v-card-media>
+        </v-card>
+      </v-flex>
+      <v-card>
+        <v-card-title primary-title>
+          <p class="text-xs-center headline" style="width: 100%">{{club.name}}</p>
+        </v-card-title>
+        <v-card-text style="margin: 10px">
+          <p>{{club.information}}</p>
+          <v-divider style="margin: 10px"></v-divider>
         </v-card-text>
-      </v-slide-y-transition>
-      <v-slide-y-transition>
-        <v-card-text v-show="showMembers">
-          <div v-for="member in members">
-            <img :src="makeInitialsImage(member)" class="initials-img" /><p class="member subheading">{{member.firstName}} {{member.lastName}} - {{member.nickname}}</p>
-          </div>
-        </v-card-text>
-      </v-slide-y-transition>
-    </v-card>
-    <p class="title text-xs-center" style="margin: 10px;">Games</p>
-    <div style="width: 100%">
-      <ul v-for="game in games" :key="game.key">
-        <game-card :game="game"></game-card>
-      </ul>
-    </div>
-  </div>
+        <v-card-actions>
+          <v-btn icon @click.native="showMembers = !showMembers; showLocation = false">
+            <v-icon>{{ showMembers ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+          </v-btn>
+          <p>Members</p>
+          <v-spacer></v-spacer>
+          <p>Location</p>
+          <v-btn icon @click.native="showLocation = !showLocation; showMembers = false">
+            <v-icon>{{ showLocation ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+          </v-btn>
+        </v-card-actions>
+        <v-slide-y-transition>
+          <v-card-text v-show="showLocation">
+            <location-view v-if="club.location !== undefined" :location="club.location"></location-view>
+          </v-card-text>
+        </v-slide-y-transition>
+        <v-slide-y-transition>
+          <v-card-text v-show="showMembers">
+            <div v-for="member in members">
+              <img :src="makeInitialsImage(member)" class="initials-img" /><p class="member subheading">{{member.firstName}} {{member.lastName}} - {{member.nickname}}</p>
+            </div>
+          </v-card-text>
+        </v-slide-y-transition>
+      </v-card>
+      <p class="title text-xs-center" style="margin: 10px;">Games</p>
+      <div style="width: 100%">
+        <ul v-for="game in games" :key="game.key">
+          <game-card :game="game"></game-card>
+        </ul>
+      </div>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -100,12 +113,22 @@
 </script>
 
 <style scoped>
-  .header-img {
+
+  .resize-parallax {
+    display: block;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: center; 
     width: 100%;
-    height: 200px;
-    object-fit: cover;
+    height: auto;
   }
 
+  @media screen and (max-width: 480px) {
+    .resize-parallax {
+      height: 200px;
+    }
+  }
   .initials-img {
     width: 40px;
     height: 40px;
