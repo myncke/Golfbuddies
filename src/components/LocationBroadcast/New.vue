154bdf4812b8faa7da06354e3c745a900d53a42a
Karel <kavdnbus.VandenBussche@ugent.be>
2017-11-21 19:49:31 +0100

    Fields are now required in the travel

--- src/components/LocationBroadcast/New.vue
+++ src/components/LocationBroadcast/New.vue
@@ -6,15 +6,19 @@
     <v-card-text>
       <v-layout row wrap>
 
+        <v-flex xs12>
+          <p class="red--text title" v-if="!valid && saved">{{validText}}</p>
+        </v-flex>
+
         <v-flex sm6 xs12>
           <date-picker class="ma-3" label="Departure Date" v-on:date-chosen="val => { model.start = new Date(val) }"
-                       :allowedFunction="(date) => date >= (new Date())"
+                       :allowedFunction="(date) => date >= addDays(-1,new Date())"
           ></date-picker>
         </v-flex>
 
         <v-flex sm6 xs12>
           <date-picker class="ma-3" label="Return Date" v-on:date-chosen="val => { model.end = new Date(val) }"
-                       :allowedFunction="(date) => date > (model.start || new Date())"
+                       :allowedFunction="(date) => date > (model.start || addDays(-1,new Date()))"
           ></date-picker>
         </v-flex>
 
@@ -42,22 +46,36 @@
   import LocationBroadcastModel from '../../models/LocationBroadcastModel'
   import MenuDatePicker from '../Shared/MenuDatePicker'
   import UserModel from '../../models/UserModel'
+  import DateUtils from '../../utils/DateUtils'
 
   export default {
     data: () => ({
-      model: undefined
+      model: undefined,
+      validText: 'Please fill in all the required information',
+      saved: false
     }),
+    computed: {
+      valid: function () {
+        return this.model.start !== undefined && this.model.end !== undefined && this.model.location !== undefined
+      }
+    },
     created: function () {
       this.model = new LocationBroadcastModel()
       this.model.userKey = this.$store.getters.user.key
     },
     methods: {
       saveModel: function () {
-        this.model.start = new Date(this.model.start)
-        this.model.end = new Date(this.model.end)
-        this.model.userKey = (new UserModel())._getDocRef()
-        this.model.save()
-        this.$router.push({name: 'locationBroadcast', params: {id: this.model.key}})
+        this.saved = true
+        if (this.valid) {
+          this.model.start = new Date(this.model.start)
+          this.model.end = new Date(this.model.end)
+          this.model.userKey = (new UserModel())._getDocRef()
+          this.model.save()
+          this.$router.push({name: 'locationBroadcast', params: {id: this.model.key}})
+        }
+      },
+      addDays (amount, date) {
+        return DateUtils.addDays(amount, date)
       }
     },
     components: {
